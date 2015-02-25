0a2e7ede38c4abc79f879f137b312bd7a7e809b9
nelsonic <contact.nelsonic@gmail.com>
2015-02-25 18:58:12 +0000

    no changes to index.js tried crypto.randomBytes(80).toString("base64") but it was *slower* than createHash! https://github.com/nelsonic/perma/issues/13

--- packages/id/test/test.js
+++ packages/id/test/test.js
@@ -26,22 +26,3 @@ test("Full Length Hash", function(t) {
   t.true(hash.length > 20, "Full Length is "+hash);
   t.end();
 });
-
-test("Performance Test", function(t) {
-  var st = new Date().getTime(); // start time
-  var str;
-  var n  = 1000000;
-  for(var i = 0; i < n; i++){
-    var length = Math.floor(Math.random() * 27) + 1
-    str = perma(i, length);
-  }
-  var et = new Date().getTime(); // end time
-  var took = et - st;            // elapsed time
-  console.log("Generated " + n + " (one million) permalinks in " + took + " ms");
-  t.true(took < 20000, " >> Performance: " + Math.floor(n/took*1000) + "/sec");
-  t.end(); // should not fail just because Travis-CI instance is low-powered!
-});
-
-var longurl = '/my-awesome-post-about-unicorns';
-var tinyurl = perma(longurl, "full");
-console.log(">> "+tinyurl + ' | '+tinyurl.length); // 89CkC
