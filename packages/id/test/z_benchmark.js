4ae4fafc8c410d11ca20f06ac438743975034354
nelsonic <contact.nelsonic@gmail.com>
2015-02-25 18:50:13 +0000

    split out the performance test into their own file - https://github.com/nelsonic/perma/issues/13

---
+++ packages/id/test/z_benchmark.js
@@ -0,0 +1,17 @@
+var perma = require('../index');
+var test  = require('tape');
+
+test("Performance Test", function(t) {
+  var st = new Date().getTime(); // start time
+  var str;
+  var n  = 1000000;
+  for(var i = 0; i < n; i++){
+    // var length = Math.floor(Math.random() * 27) + 1
+    str = perma(i,5);
+  }
+  var et = new Date().getTime(); // end time
+  var took = et - st;            // elapsed time
+  console.log("Generated " + n + " (one million) permalinks in " + took + " ms");
+  t.true(took < 20000, " >> Performance: " + Math.floor(n/took*1000) + "/sec");
+  t.end(); // should not fail just because Travis-CI instance is low-powered!
+});
