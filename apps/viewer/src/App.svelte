35bbf3a449c12c4b6c853ee2dca0964707f5c1d6
Bert Spaan <bert.spaan@gmail.com>
2021-01-23 19:14:21 +0100

    added main logic + query parsing

--- apps/viewer/src/App.svelte
+++ apps/viewer/src/App.svelte
@@ -1,25 +1,46 @@
 <script>
-	export let name;
+	import queryString from 'query-string'
+
+	let queryParams
+
+	export let url
+	export let annotation
+
+
+	$: queryParams = queryString.parse(window.location.search)
+	$: url = queryParams.url
+
+	async function fetchAnnotation (url) {
+		console.log('nu', url)
+		const response = await fetch(url)
+
+		// TODO: check annotation!
+		annotation = await response.json()
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#if url}
+		{#await fetchAnnotation(url)}
+			<p>...waiting</p>
+		{:then data}
+			<pre><code>{JSON.stringify(annotation, null, 2)}</code></pre>
+		{:catch error}
+			<p>An error occurred!</p>
+		{/await}
+	{:else}
+		<form>
+			<input name="url" placeholder="Annotation URL" autofocus />
+			<button>Go</button>
+		</form>
+	{/if}
 </main>
 
 <style>
 	main {
-		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
