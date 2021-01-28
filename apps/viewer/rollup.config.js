d74f47403d8f6e00e44b7a0028e491e161f23250
Bert Spaan <bert.spaan@gmail.com>
2021-01-28 13:15:51 +0100

    fixed rollup code style

--- apps/viewer/rollup.config.js
+++ apps/viewer/rollup.config.js
@@ -16,7 +16,7 @@ function serve () {
 
   return {
     writeBundle () {
-      if (server) return;
+      if (server) return
       server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
         stdio: ['ignore', 'inherit', 'inherit'],
         shell: true
@@ -68,7 +68,7 @@ export default {
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
-    // production && terser()
+    production && terser()
   ],
   watch: {
     clearScreen: false
