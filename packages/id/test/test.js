7be609a7ccee50e1547b1ae4b4e9bbdb8c073795
Bert Spaan <bert.spaan@gmail.com>
2021-07-26 15:48:53 +0200

    converted tests to chai

--- packages/id/test/test.js
+++ packages/id/test/test.js
@@ -1,64 +1,86 @@
-import test from 'tape'
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
 
 import { createId, createRandomId, createChecksum } from '../index.js'
 
-test('Create ID for url: 1234', async (t) => {
+const DEFAULT_LENGTH = 16
+
+describe('Create ID for url "1234"', async () => {
   const id = await createId(1234)
-  t.equal(id.length, 16, 'Worked as expected ' + id)
-  t.equal(id, '1ARVn2Auq2WAqx2g', 'id is consistent. 1234 >> 1ARVn2Auq2WAqx2g')
-  t.end()
-})
 
-test('Confirm string characters are in allowed chars', async (t) => {
-  const charSet = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz123456789'
-  const id = await createId(Math.random())
-  console.log('Our Random string is: ' + id)
-
-  let char
-  id.split('').map((c) => {
-    char = c
+  it(`ID should be ${DEFAULT_LENGTH} characters long`, () => {
+    expect(id.length).to.equal(DEFAULT_LENGTH)
   })
 
-  t.true(charSet.indexOf(char) > -1, char + ' is in charSet: ' + charSet)
-  t.end()
+  const expectedId = '1ARVn2Auq2WAqx2g'
+  it(`ID should equal "${expectedId}"`, () => {
+    expect(id).to.equal(expectedId)
+  })
 })
 
-test('Create random IDs', async (t) => {
+// test('Confirm string characters are in allowed chars', async (t) => {
+//   const charSet = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz123456789'
+//   const id = await createId(Math.random())
+//   console.log('Our Random string is: ' + id)
+
+//   let char
+//   id.split('').map((c) => {
+//     char = c
+//   })
+
+//   t.true(charSet.indexOf(char) > -1, char + ' is in charSet: ' + charSet)
+//   t.end()
+// })
+
+describe('Create random IDs', async () => {
   const id1 = await createRandomId()
   const id2 = await createRandomId()
-  t.equal(id1.length, 16, 'Worked as expected ' + id1)
-  t.notEqual(id1, id2, 'IDs are different')
-  t.end()
+
+  it(`IDs should be ${DEFAULT_LENGTH} characters long`, () => {
+    expect(id1.length).to.equal(DEFAULT_LENGTH)
+    expect(id2.length).to.equal(DEFAULT_LENGTH)
+  })
+
+  it('IDs should be different', () => {
+    expect(id1).to.not.equal(id2)
+  })
 })
 
-test('Full Length Hash', async (t) => {
-  const id = await createId('qwertyuiopasdfghjklzxcvbnm', 50)
-  t.true(id.length > 20, 'Full Length is ' + id)
-  t.end()
+const length = 50
+describe(`ID of length ${length}`, async () => {
+  const id = await createId('qwertyuiopasdfghjklzxcvbnm', length)
+
+  it(`ID should have length ${length}`, () => {
+    expect(id.length).to.equal(length)
+  })
 })
 
-const obj1 = {
-  a: 1,
-  b: 2,
-  c: [1, 2, 3, 4, 5]
-}
+describe('Object checksum', async () => {
+  const obj1 = {
+    a: 1,
+    b: 2,
+    c: [1, 2, 3, 4, 5]
+  }
 
-const obj2 = {
-  c: [1, 2, 3, 4, 5],
-  b: 2,
-  a: 1
-}
+  const obj2 = {
+    c: [1, 2, 3, 4, 5],
+    b: 2,
+    a: 1
+  }
 
-test('Object checksum', async (t) => {
-  const checksum = await createChecksum(obj1)
-  t.equal(checksum.length, 16, 'Worked as expected ' + checksum)
-  t.equal(checksum, 'f2XwC5sWfSCWd9mJ', 'checksum is correct')
-  t.end()
-})
-
-test('Semantically equal objects produce equal checksums', async (t) => {
   const checksum1 = await createChecksum(obj1)
   const checksum2 = await createChecksum(obj2)
-  t.equal(checksum1, checksum2, 'checksums are equal')
-  t.end()
+
+  it(`Checksums should have length ${DEFAULT_LENGTH}`, () => {
+    expect(checksum1.length).to.equal(DEFAULT_LENGTH)
+    expect(checksum2.length).to.equal(DEFAULT_LENGTH)
+  })
+
+  it('Checksum should be correct', () => {
+    expect(checksum1).to.equal('f2XwC5sWfSCWd9mJ')
+  })
+
+  it('Semantically equal objects produce equal checksums', () => {
+    expect(checksum1).to.equal(checksum2)
+  })
 })
