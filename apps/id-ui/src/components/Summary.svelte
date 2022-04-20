015e4f5db67cd3d1599f74ec3d3933b724d26d7f
Bert Spaan <bert.spaan@gmail.com>
2022-04-20 12:09:48 +0200

    using new version of @allmaps/id

--- apps/id-ui/src/components/Summary.svelte
+++ apps/id-ui/src/components/Summary.svelte
@@ -12,6 +12,6 @@
     Its URI is <code>{parsedIiif.uri}</code>.
   </div>
   <div>
-    Its Allmaps ID is <code>{id}</code>. This ID is computed by taking the first 16 hexadecimal characters of the <a href="https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest">SHA-512 hash</a> of the URI.
+    Its Allmaps ID is <code>{id}</code>. This ID is computed by taking the first 16 hexadecimal characters of the <a href="https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest">SHA-1 hash</a> of the URI.
   </div>
 </div>
