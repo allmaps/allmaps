90d965d2f466e7298b6c7ee3ffac5ff49494d5b3
Bert Spaan <bert.spaan@gmail.com>
2022-06-09 10:49:20 +0200

    gitignore dist dir

--- packages/id/dist/web.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function generateId(str: string, length?: number): Promise<string>;
-export declare function generateRandomId(length?: number): Promise<string>;
-export declare function generateChecksum(obj: {}, length?: number): Promise<string>;
