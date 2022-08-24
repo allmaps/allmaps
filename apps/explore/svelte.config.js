05e27b5ebb3d73d1caa060469574cad8e496b5fc
Bert Spaan <bert.spaan@gmail.com>
2022-08-24 14:33:37 +0200

    using adapter-static

--- apps/explore/svelte.config.js
+++ apps/explore/svelte.config.js
@@ -1,15 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static'
+import preprocess from 'svelte-preprocess'
+
+// const dev = process.env.NODE_ENV === 'development'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
-};
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false
+    }),
+    prerender: {
+      default: true
+    }
+  }
+}
 
-export default config;
+export default config
