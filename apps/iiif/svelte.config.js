9231a8133c18b5db90d3e5adb6744a004cb3bcaa
Bert Spaan <bert.spaan@gmail.com>
2022-10-19 09:07:38 +0200

    updated config

--- apps/iiif/svelte.config.js
+++ apps/iiif/svelte.config.js
@@ -1,12 +1,8 @@
 import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 
-// const dev = process.env.NODE_ENV === 'development'
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
   preprocess: preprocess({
     postcss: true
   }),
@@ -15,7 +11,7 @@ const config = {
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: null,
+      fallback: 'index.html',
       precompress: false
     })
   }
