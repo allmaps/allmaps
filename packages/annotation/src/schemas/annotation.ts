9b5c6e4de60933f6dda99e398aba4715c209e0e1
Bert Spaan <bert.spaan@gmail.com>
2022-10-24 20:52:23 +0200

    allow negative coordinate values for pixel mask

--- packages/annotation/src/schemas/annotation.ts
+++ packages/annotation/src/schemas/annotation.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 import { PointSchema, ImageServiceSchema } from './shared.js'
 
 const svg =
-  /^<svg\s+width="\d+"\s+height="\d+"\s*>\s*<polygon\s+points="\s*(\d+(\.\d+)?,\d+(\.\d+)?\s+){2,}\d+(\.\d+)?,\d+(\.\d+)?\s*"\s*\/>\s*<\/svg>$/
+  /^<svg\s+width="\d+"\s+height="\d+"\s*>\s*<polygon\s+points="\s*(-?\d+(\.\d+)?,-?\d+(\.\d+)?\s+){2,}-?\d+(\.\d+)?,-?\d+(\.\d+)?\s*"\s*\/>\s*<\/svg>$/
 
 export const SvgSelectorSchema = z.object({
   type: z.literal('SvgSelector'),
