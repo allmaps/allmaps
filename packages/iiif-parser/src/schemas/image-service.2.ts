9049ffd2f46928c289734e5b80259cd823672946
Bert Spaan <bert.spaan@gmail.com>
2022-11-26 10:07:57 +0100

    added sfo museum test case, added support for presentation api 3 with image api 2

--- packages/iiif-parser/src/schemas/image-service.2.ts
+++ packages/iiif-parser/src/schemas/image-service.2.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod'
 
-import { Image2ProfileSchema} from './image.2.js'
+import { Image2ProfileSchema } from './image.2.js'
+
+// TODO: also support Presentation API 2 with Image API 3
 
 export const ImageService2Schema = z.object({
   '@id': z.string().url(),
--- packages/iiif-parser/src/schemas/image-service.3.ts
+++ packages/iiif-parser/src/schemas/image-service.3.ts
@@ -1,13 +1,24 @@
 import { z } from 'zod'
 
+import { Image2ProfileSchema } from './image.2.js'
+
 export const complianceLevels = ['level0', 'level1', 'level2'] as const
 
 export const imageServiceTypes = ['ImageService2', 'ImageService3'] as const
 
-complianceLevels
+export const Presentation3ImageService2Schema = z.object({
+  id: z.string().url(),
+  type: z.literal('ImageService2'),
+  profile: Image2ProfileSchema
+})
 
-export const ImageService3Schema = z.object({
+export const Presentation3ImageService3Schema = z.object({
   id: z.string().url(),
   type: z.enum(imageServiceTypes),
   profile: z.enum(complianceLevels)
 })
+
+export const ImageService3Schema = z.union([
+  Presentation3ImageService2Schema,
+  Presentation3ImageService3Schema
+])
