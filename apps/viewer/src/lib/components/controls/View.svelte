b09574ee385cee9002afcdc7d5f667423b8a6bc7
Bert Spaan <bert.spaan@gmail.com>
2023-05-05 14:55:09 +0200

    show active view in control

--- apps/viewer/src/lib/components/controls/View.svelte
+++ apps/viewer/src/lib/components/controls/View.svelte
@@ -6,7 +6,8 @@
   <button
     on:click={() => ($view = 'map')}
     aria-current={$view === 'map' ? 'page' : 'false'}
-    class="px-4 py-2 text-sm text-gray-900 font-medium bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+    class="px-4 py-2 text-sm font-medium bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700"
+    class:text-gray-900={$view !== 'map'}
     class:text-blue-700={$view === 'map'}
   >
     Map
@@ -15,7 +16,8 @@
   <button
     on:click={() => ($view = 'list')}
     aria-current={$view === 'list' ? 'page' : 'false'}
-    class="px-4 py-2 text-sm text-gray-900 font-medium bg-white border border-gray-200  hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+    class="px-4 py-2 text-sm font-medium bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700"
+    class:text-gray-900={$view !== 'list'}
     class:text-blue-700={$view === 'list'}
   >
     List
@@ -24,7 +26,8 @@
   <button
     on:click={() => ($view = 'image')}
     aria-current={$view === 'image' ? 'page' : 'false'}
-    class="px-4 py-2 text-sm text-gray-900 font-medium bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+    class="px-4 py-2 text-sm font-medium bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700"
+    class:text-gray-900={$view !== 'image'}
     class:text-blue-700={$view === 'image'}
   >
     Image
