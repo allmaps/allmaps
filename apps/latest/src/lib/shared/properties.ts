644c59e0b4160feca7b1c44e1920b008f7e67f7d
Bert Spaan <bert.spaan@gmail.com>
2023-08-16 21:39:28 +0200

    ignore typescript eslint error

--- apps/latest/src/lib/shared/properties.ts
+++ apps/latest/src/lib/shared/properties.ts
@@ -43,6 +43,7 @@ function formatTimeAgo(dateStr?: string) {
 export function getProperties(map: Map, polygon?: Polygon) {
   // TODO: create new type for API output
   // maybe with https://github.com/trpc/trpc
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const updatedAt = (map as any).updatedAt as string
 
   const { hostname } = new URL(map.image.uri)
