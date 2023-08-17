06a820b600f2d4d7a9b875272815ff18114ae326
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2023-08-17 15:07:01 +0200

    Triangulate: tsdocs

--- packages/triangulate/src/index.ts
+++ packages/triangulate/src/index.ts
@@ -4,7 +4,30 @@ import classifyPoint from 'robust-point-in-polygon'
 import * as poly2tri from 'poly2tri'
 import type { SVGPolygon, Position } from '@allmaps/types'
 
-// use this function to access the ritch poly2tri triangulaiton output (information on constrained edges, neighbours, interiour, ...)
+/**
+ * Triangle as `[[x0, y0], [x1, y1], [x2, y2]]`
+ * @typedef {Object} Triangle
+ */
+
+/**
+ * Triangle object from [poly2tri](https://github.com/r3mi/poly2tri.js/) package
+ * @typedef {Object} poly2tri.Triangle
+ */
+
+/**
+ * Polygon object as `[[number, number], ...]`
+ * @typedef {Object} SVGPolygon
+ */
+
+/**
+ * Triangulates a polygon (and returns the full Poly2tri output)
+ *
+ * @remark Use this function to access the ritch poly2tri triangulation output (information on constrained edges, neighbours, interiour, ...).
+ *
+ * @param {SVGPolygon} polygon - Polygon
+ * @param {number} distance - Distance between the Steiner points placed in a grid inside the polygon
+ * @returns {poly2tri.Triangle[]} Array of triangles partitionning the polygon
+ */
 export function triangulatePoly2tri(polygon: SVGPolygon, distance: number) {
   // create grid
   const grid: Position[] = createGrid(polygon, distance)
@@ -28,8 +51,15 @@ export function triangulatePoly2tri(polygon: SVGPolygon, distance: number) {
   return swctx.getTriangles()
 }
 
-// use this function to get an array of triangles
-// returns [[[t1x1, t1y1], [t1x2, t1y2], [t1x3, t1y3]], [[t2x1, t2y1], [t2x2, t2y2], [t2x3, t2y3]], ...]
+/**
+ * Triangulates a polygon
+ *
+ * @remark Polygons with < 3 points just return an empty array.
+ *
+ * @param {SVGPolygon} polygon - Polygon
+ * @param {number} distance - Distance between the Steiner points placed in a grid inside the polygon
+ * @returns {Triangle[]} Array of triangles partitionning the polygon
+ */
 export function triangulate(polygon: SVGPolygon, distance: number) {
   return triangulatePoly2tri(polygon, distance).map((t) =>
     t.getPoints().map((p) => [p.x, p.y])
