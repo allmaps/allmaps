999265ec50cf177147b7a54b67fcbe260cce66c0
Bert Spaan <bert.spaan@gmail.com>
2023-08-29 16:37:41 +0200

    updated docs deps, this fixed loading tailwind classes from @allmaps/ui

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -5,8 +5,7 @@ import { theme } from '@allmaps/tailwind'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    // './node_modules/@allmaps/ui/dist/components/MapMonster.svelte',
-    // './node_modules/@allmaps/ui/dist/components/**/*.{svelte,ts}'
+    './node_modules/@allmaps/ui/dist/components/**/*.{svelte,ts}',
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
   ],
   theme,
