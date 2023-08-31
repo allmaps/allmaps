ebcf4f0bd6bf6dd59c1c6119fdb6b91247fe1e29
Bert Spaan <bert.spaan@gmail.com>
2023-08-31 07:21:19 +0200

    only copy readmes when valid, and copy readmes before build

--- apps/docs/scripts/copy-readmes.js
+++ apps/docs/scripts/copy-readmes.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+// @ts-check
+
 import url from 'url'
 import path from 'path'
 import { readdir, readFile, writeFile } from 'fs/promises'
@@ -8,6 +10,10 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 const packagesDir = path.join(__dirname, '../../../packages')
 const outputDir = path.join(__dirname, '../src/content/docs/reference/packages')
+
+function isReadmeValid(readme) {
+  return readme.length > 150
+}
 
 const getDirectories = async (source) =>
   (await readdir(source, { withFileTypes: true }))
@@ -19,6 +25,11 @@ const removeFirstLine = (lines) => lines.substring(lines.indexOf('\n') + 1)
 for (const packageName of await getDirectories(packagesDir)) {
   const readmePath = path.join(packagesDir, packageName, 'README.md')
   const readme = await readFile(readmePath, { encoding: 'utf8' })
+
+  if (!isReadmeValid(readme)) {
+    console.log('Skipping README.md from', readmePath)
+    continue
+  }
 
   console.log('Copying README.md from', readmePath)
 
