e04493c31e0fe497d5b2f97a641646c9b5faa9d4
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2023-09-14 12:43:56 +0200

    WIP: transform polygon refactor: stdlib test function chai with types

--- packages/stdlib/src/test.ts
+++ packages/stdlib/src/test.ts
@@ -2,14 +2,18 @@ import { expect } from 'chai'
 
 const EPSILON = 0.00001
 
-export function expectToBeCloseToArray(actual, expected, epsilon = EPSILON) {
+export function expectToBeCloseToArray(
+  actual: number[],
+  expected: number[],
+  epsilon = EPSILON
+) {
   expect(actual.length).to.equal(expected.length)
   actual.forEach((n, i) => expect(n).to.be.approximately(expected[i], epsilon))
 }
 
 export function expectToBeCloseToArrayArray(
-  actual,
-  expected,
+  actual: number[][],
+  expected: number[][],
   epsilon = EPSILON
 ) {
   expect(actual.length).to.equal(expected.length)
