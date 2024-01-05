43797f326126b90954b5f5eb01f19effde77b9f3
Bert Spaan <bert.spaan@gmail.com>
2024-01-05 23:10:35 +0100

    fixed tests for @allmaps/id

--- packages/id/test/test.js
+++ packages/id/test/test.js
@@ -9,34 +9,33 @@ import {
 
 const DEFAULT_LENGTH = 16
 
-describe('generate ID for string "1234"', async () => {
-  const id = await generateId(1234)
+const id1234 = await generateId(1234)
 
-  it(`ID should be ${DEFAULT_LENGTH} characters long`, () => {
-    expect(id.length).to.equal(DEFAULT_LENGTH)
+describe('generate ID for string "1234"', () => {
+  it(`ID should be ${DEFAULT_LENGTH} characters long`, async () => {
+    expect(id1234.length).to.equal(DEFAULT_LENGTH)
   })
 
   const expectedId = '7110eda4d09e062a'
-  it(`ID should equal "${expectedId}"`, () => {
-    expect(id).to.equal(expectedId)
+  it(`ID should equal "${expectedId}"`, async () => {
+    expect(id1234).to.equal(expectedId)
   })
 })
 
 const iiifUrl = 'https://polona.pl/iiif/item/NTU5NTE4OTg/manifest.json'
-describe(`generate ID for URL "${iiifUrl}"`, async () => {
-  const id = await generateId(iiifUrl)
-
+describe(`generate ID for URL "${iiifUrl}"`, () => {
   const expectedId = '8520a34527aba090'
-  it(`ID should equal "${expectedId}"`, () => {
+  it(`ID should equal "${expectedId}"`, async () => {
+    const id = await generateId(iiifUrl)
     expect(id).to.equal(expectedId)
   })
 })
 
-describe('generate random IDs', async () => {
-  const id1 = await generateRandomId()
-  const id2 = await generateRandomId()
+const id1 = await generateRandomId()
+const id2 = await generateRandomId()
 
-  it(`IDs should be ${DEFAULT_LENGTH} characters long`, () => {
+describe('generate random IDs', () => {
+  it(`IDs should be ${DEFAULT_LENGTH} characters long`, async () => {
     expect(id1.length).to.equal(DEFAULT_LENGTH)
     expect(id2.length).to.equal(DEFAULT_LENGTH)
   })
@@ -48,30 +47,30 @@ describe('generate random IDs', async () => {
 
 const length = 50
 const maxLength = 40
-describe(`ID of length ${length}`, async () => {
-  const id = await generateId('qwertyuiopasdfghjklzxcvbnm', length)
 
+const id = await generateId('qwertyuiopasdfghjklzxcvbnm', length)
+describe(`ID of length ${length}`, () => {
   it(`ID should have length ${maxLength}`, () => {
     expect(id.length).to.equal(maxLength)
   })
 })
 
-describe('Object checksum', async () => {
-  const obj1 = {
-    a: 1,
-    b: 2,
-    c: [1, 2, 3, 4, 5]
-  }
+const obj1 = {
+  a: 1,
+  b: 2,
+  c: [1, 2, 3, 4, 5]
+}
 
-  const obj2 = {
-    c: [1, 2, 3, 4, 5],
-    b: 2,
-    a: 1
-  }
+const obj2 = {
+  c: [1, 2, 3, 4, 5],
+  b: 2,
+  a: 1
+}
 
-  const checksum1 = await generateChecksum(obj1)
-  const checksum2 = await generateChecksum(obj2)
+const checksum1 = await generateChecksum(obj1)
+const checksum2 = await generateChecksum(obj2)
 
+describe('Object checksum', () => {
   it(`Checksums should have length ${DEFAULT_LENGTH}`, () => {
     expect(checksum1.length).to.equal(DEFAULT_LENGTH)
     expect(checksum2.length).to.equal(DEFAULT_LENGTH)
