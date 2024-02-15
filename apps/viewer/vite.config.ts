38b04d239f13f86dbcfc3c2c331ed2fa588a28eb
Christopher Pietsch <cpietsch@gmail.com>
2024-02-15 14:44:32 +0100

    added dummy https support for viewer vite to enable Crypto: subtle property

--- apps/viewer/vite.config.ts
+++ apps/viewer/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 import { sveltekit } from '@sveltejs/kit/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 import ports from '../../ports.json'
 
@@ -8,5 +9,5 @@ export default defineConfig({
   server: {
     port: ports.viewer
   },
-  plugins: [sveltekit()]
+  plugins: [sveltekit(), basicSsl()]
 })
