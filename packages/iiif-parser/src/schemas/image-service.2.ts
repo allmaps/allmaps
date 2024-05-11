70081b1ab8010b9d091e21aed5c513a074be9696
Bert Spaan <bert.spaan@gmail.com>
2024-05-11 13:21:48 +0200

    made iiif-parser less strict to be able to parse Delft manifests

--- packages/iiif-parser/src/schemas/image-service.2.ts
+++ packages/iiif-parser/src/schemas/image-service.2.ts
@@ -1,12 +1,16 @@
 import { z } from 'zod'
 import { Image1ProfileSchema, Image1Context } from './image.1.js'
 import { Image2ProfileSchema, Image2Context } from './image.2.js'
+import { Image3ProfileSchema } from './image.3.js'
+
+import { ImageServiceTypesSchema } from './shared.js'
 
 // TODO: also support Presentation API 2 with Image API 3
 // TODO: also support Image3Context (a string or an array of strings)
 export const ImageService2Schema = z.object({
   '@id': z.string().url(),
-  profile: Image1ProfileSchema.or(Image2ProfileSchema),
+  '@type': ImageServiceTypesSchema.optional(),
+  profile: Image1ProfileSchema.or(Image2ProfileSchema).or(Image3ProfileSchema),
   width: z.number().int().optional(),
   height: z.number().int().optional(),
   '@context': Image1Context.or(
