c095abc1b8c38bd5f38f5f8f211053176735df98
Bert Spaan <bert.spaan@gmail.com>
2024-05-11 14:15:35 +0200

    added container queriea + tailwind plugin

--- packages/ui/src/lib/components/Collection.svelte
+++ packages/ui/src/lib/components/Collection.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 </script>
 
-<ol
-  class="grid gap-3 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 @md:grid-cols-3 @lg:grid-cols-4 @xl:grid-cols-5"
->
-  <slot />
-</ol>
+<div class="w-full @container">
+  <ol
+    class="grid gap-3 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 @3xl:grid-cols-3 @5xl:grid-cols-4 @7xl:grid-cols-5"
+  >
+    <slot />
+  </ol>
+</div>
 
 <style scoped>
 </style>
