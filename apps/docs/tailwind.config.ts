95000bbb00ec53fcbd1b7a4db46aa2ee97f094b3
Bert Spaan <bert.spaan@gmail.com>
2024-05-29 09:27:45 +0200

    converted tailwind config tile to ts for docs

--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -9,12 +9,5 @@ export default {
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
   ],
   theme,
-  // extend: {
-  //   colors: {
-  //     // Your preferred accent color. Indigo is closest to Starlight’s defaults.
-  //     accent: colors.indigo,
-  //     // Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-  //     gray: colors.zinc,
-  //   },
   plugins: [starlightPlugin()]
 }
