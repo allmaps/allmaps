09f495417599305bad3e24d1a0d692fb9cca1dff
Bert Spaan <bert.spaan@gmail.com>
2024-05-29 13:24:11 +0200

    added container queries to collection component, with !important modifier to enfore them over media queries

--- packages/ui/src/lib/components/Collection.svelte
+++ packages/ui/src/lib/components/Collection.svelte
@@ -1,6 +1,9 @@
+<!-- TODO: is the the !important modifier for the container queries really needed?
+  Is there another way to make sure container queries have precedence over media queries? -->
 <div class="w-full @container">
   <ol
-    class="grid gap-3 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 @3xl:grid-cols-3 @5xl:grid-cols-4 @7xl:grid-cols-5"
+    class="grid gap-3 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5
+      @xl:!grid-cols-3 @5xl:!grid-cols-4 @7xl:!grid-cols-5"
   >
     <slot />
   </ol>
