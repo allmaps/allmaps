c6be6c94a12616c3fb39eb572045f454479d64d1
Brandon Liu <bdon@bdon.org>
2024-06-29 12:32:38 +0200

    update explore page basemap
    * update maplibre, protomaps dependencies
    * add contour lines and hillshade

--- apps/explore/src/routes/+page.svelte
+++ apps/explore/src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import { onMount } from 'svelte'
 
+  import maplibregl from 'maplibre-gl'
   import { Map, addProtocol } from 'maplibre-gl'
   import layers from 'protomaps-themes-base'
   import { Protocol } from 'pmtiles'
+  import { default as mlcontour } from 'maplibre-contour'
 
   import { Header, Thumbnail, Stats } from '@allmaps/ui'
   import { fetchImageInfo } from '@allmaps/stdlib'
@@ -79,6 +81,10 @@
   onMount(() => {
     const protocol = new Protocol()
     addProtocol('pmtiles', protocol.tile)
+    var demSource = new mlcontour.DemSource({
+        url: "https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png",
+      });
+    demSource.setupMaplibre(maplibregl);
 
     map = new Map({
       container,
@@ -86,6 +92,8 @@
         version: 8,
         glyphs:
           'https://protomaps.github.io/basemaps-assets/fonts/{fontstack}/{range}.pbf',
+        sprite:
+          'https://protomaps.github.io/basemaps-assets/sprites/v3/light',
         sources: {
           protomaps: {
             type: 'vector',
@@ -107,6 +115,59 @@
     })
 
     map.on('load', () => {
+      map.addSource("terrain", {
+          type: 'raster-dem',
+          tiles: [demSource.sharedDemProtocolUrl],
+          maxzoom: 13,
+          encoding: "terrarium",
+          attribution: "<a href='https://github.com/tilezen/joerd/tree/master'>Joerd</a>"
+        });
+
+      map.addSource("contour-source", {
+        type: "vector",
+        tiles: [
+          demSource.contourProtocolUrl({
+            thresholds: {
+              // zoom: [minor, major]
+              11: [200, 1000],
+              12: [100, 500],
+              14: [50, 200],
+              15: [20, 100],
+            },
+            // optional, override vector tile parameters:
+            contourLayer: "contours",
+            elevationKey: "ele",
+            levelKey: "level",
+            extent: 4096,
+            buffer: 1,
+          }),
+        ],
+        maxzoom: 15,
+      });
+
+      map.addLayer({
+        id: "hillshade",
+        type: "hillshade",
+        source: "terrain",
+        paint: {
+          "hillshade-exaggeration": 0.6,
+          "hillshade-shadow-color": "#bbb",
+          "hillshade-highlight-color": "white",
+          "hillshade-accent-color": "green"
+        }
+      },"water")
+
+      map.addLayer({
+        id: "contour-lines",
+        type: "line",
+        source: "contour-source",
+        "source-layer": "contours",
+        paint: {
+          // level = highest index in thresholds array the elevation is a multiple of
+          "line-width": ["match", ["get", "level"], 1, 1, 0.5],
+        },
+      }, "water");
+
       map.addSource('masks', {
         type: 'vector',
         url: `pmtiles://${pmtilesUrl}`
