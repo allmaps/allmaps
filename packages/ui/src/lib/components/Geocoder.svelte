6ede63d8543cdebe6a9aeb831bf080817364287a
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2024-07-07 12:45:04 +0200

    Geocoder: add WHG, improve functionality

--- packages/ui/src/lib/components/Geocoder.svelte
+++ packages/ui/src/lib/components/Geocoder.svelte
@@ -1,50 +1,48 @@
 <script lang="ts">
   import { throttle } from 'lodash-es'
-  import { createEventDispatcher } from 'svelte'
+  import { onMount, createEventDispatcher } from 'svelte'
 
   import { PUBLIC_GEOCODE_EARTH_API_KEY } from '$env/static/public'
 
   import searchIcon from '$lib/shared/images/search.svg'
 
-  import type { GeojsonPoint } from '@allmaps/types'
+  import type { GeojsonPoint, GeojsonGeometry } from '@allmaps/types'
 
-  type GeoJsonFeatureGE = GeojsonPoint & { properties: { label: string } }
+  type GeoJsonFeatureGE = {
+    geometry: GeojsonPoint
+    properties: { label: string }
+  }
+  type GeoJsonFeatureWHG = {
+    geometry?: GeojsonGeometry
+    properties: { title: string }
+  }
+  // WHG return 'GeoJSON features', but not necessarilly points, and sometimes without geometry.
+  type GeoJsonFeature = GeoJsonFeatureGE
 
   export const focusPointLon: number | undefined = undefined
   export const focusPointLat: number | undefined = undefined
 
   let searchTerm = ''
-  let features: GeoJsonFeatureGE[] = []
+  let featuresGE: GeoJsonFeatureGE[] = []
+  let featuresWHG: GeoJsonFeatureWHG[] = []
+  let features: GeoJsonFeature[] = []
   // Usage: this returns a GeoJSON of with the following info
   // - properties.name => short location description
   // - properties.label => long location description
   // - geometry.coordinates => coordinates
-  let selectedFeature: GeoJsonFeatureGE | undefined
+  let selectedFeature: GeoJsonFeature | undefined
   let softFocusIndex = -1
   let softFocusedElement: HTMLButtonElement | undefined
+
+  let geocoderPopover: any
+  onMount(() => {
+    geocoderPopover = document.getElementById('geocoder-popover')
+  })
 
   const THROTTLE_WAIT_MS = 200
   const THROTTLE_OPTIONS = {
     leading: true,
     trailing: true
-  }
-
-  async function getFeatures(text = ''): Promise<void> {
-    try {
-      let query =
-        `https://api.geocode.earth/v1/autocomplete` +
-        `?api_key=${PUBLIC_GEOCODE_EARTH_API_KEY}` +
-        `&text=${text}`
-      if (focusPointLon && focusPointLat) {
-        query += `&focus.point.lon=${focusPointLon}&focus.point.lat=${focusPointLat}`
-      }
-      features = await fetch(query)
-        .then((response) => response.json())
-        .then((response) => response.features)
-    } catch (error) {
-      console.error('Error fetching geocoder:', error)
-      return
-    }
   }
 
   const throttledGetFeatures = throttle(
@@ -53,40 +51,98 @@
     THROTTLE_OPTIONS
   )
 
-  // Using if to avoid calling `fetch` eagerly during server side rendering
-  $: promise = searchTerm ? throttledGetFeatures(searchTerm) : undefined
+  function getFeatures(text: string): void {
+    if (text == '') {
+      // Using if to avoid calling `fetch` eagerly during server side rendering
+      featuresGE = []
+      featuresWHG = []
+      return
+    }
+    getFeaturesGE(text)
+    getFeaturesWHG(text)
+  }
 
-  const dispatch = createEventDispatcher()
+  async function getFeaturesGE(text: string): Promise<void> {
+    try {
+      let query =
+        `https://api.geocode.earth/v1/autocomplete` +
+        `?api_key=${PUBLIC_GEOCODE_EARTH_API_KEY}` +
+        `&text=${text}`
+      if (focusPointLon && focusPointLat) {
+        query += `&focus.point.lon=${focusPointLon}&focus.point.lat=${focusPointLat}`
+      }
+      featuresGE = await fetch(query)
+        .then((response) => response.json())
+        .then((response) => response.features)
+    } catch (error) {
+      console.error('Error fetching geocode.earth:', error)
+      return
+    }
+  }
 
-  $: dispatch('features', features)
-  $: dispatch('select', selectedFeature)
+  async function getFeaturesWHG(text: string): Promise<void> {
+    try {
+      let query = `https://whgazetteer.org/api/index/?name=${text}`
+      featuresWHG = await fetch(query)
+        .then((response) => response.json())
+        .then((response) => response.features)
+    } catch (error) {
+      console.error('Error fetching World Historical Gazetteer:', error)
+      // Strangly WHG seems to crash for some common search terms
+      // Example: https://whgazetteer.org/api/index/?name=London
+      return
+    }
+  }
+
+  $: throttledGetFeatures(searchTerm)
+
+  $: features = [
+    ...featuresGE.map(({ geometry, properties }) => ({ geometry, properties })),
+    ...featuresWHG
+      .filter((feature) => feature.geometry?.type == 'Point')
+      .map(({ geometry, properties }) => ({
+        geometry: geometry as GeojsonPoint,
+        properties: { label: properties.title }
+      }))
+  ].slice(0, 5)
+
+  function handleClick(feature: GeoJsonFeature): void {
+    selectedFeature = feature
+    geocoderPopover.hidePopover()
+    // This doesn't seem to work when the click comes from an 'Enter'
+  }
 
   function handleKeydown(event: KeyboardEvent) {
+    const resultElements = [
+      ...document.querySelectorAll('.result')
+    ] as HTMLButtonElement[]
     if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
       event.preventDefault()
-      const resultElements = [
-        ...document.querySelectorAll('.result')
-      ] as HTMLButtonElement[]
-      for (const resultElement of resultElements) {
-        resultElement.parentElement?.classList.remove('softFocus')
-      }
-
       if (event.key === 'ArrowDown') {
-        softFocusIndex = (softFocusIndex + 1) % resultElements.length
+        softFocusIndex = (softFocusIndex + 1) % features.length
       } else if (event.key === 'ArrowUp') {
-        softFocusIndex =
-          (softFocusIndex - 1 + resultElements.length) % resultElements.length
+        if (softFocusIndex == -1) {
+          softFocusIndex = features.length - 1
+        } else {
+          softFocusIndex =
+            (softFocusIndex - 1 + features.length) % features.length
+        }
       }
-
-      softFocusedElement = resultElements[softFocusIndex]
-      softFocusedElement?.scrollIntoView({ block: 'nearest' })
-      softFocusedElement?.parentElement?.classList.add('softFocus')
+      // softFocusedElement = resultElements[softFocusIndex]
+      // softFocusedElement?.scrollIntoView({ block: 'nearest' })
+      // Keeping this here in case we need to implement scrollIntoView for small screens
     } else if (event.key === 'Enter') {
+      softFocusedElement = resultElements[softFocusIndex]
       softFocusedElement?.click()
     } else {
       softFocusIndex = -1
     }
   }
+
+  const dispatch = createEventDispatcher()
+
+  $: dispatch('features', features)
+  $: dispatch('select', selectedFeature)
 </script>
 
 <svelte:window on:keydown={handleKeydown} />
@@ -108,29 +164,21 @@
   />
 
   <div class="results">
-    {#if searchTerm}
-      <ul>
-        {#await promise}
-          <!-- Only the most recent promise is considered, meaning we don't need to worry about race conditions. -->
-          <li>...searching</li>
-        {:then}
-          {#each features.slice(0, 5) as feature}
-            <li>
-              <button
-                on:click={() => {
-                  selectedFeature = feature
-                }}
-                class="result"
-                tabindex="0"
-              >
-                {feature.properties.label}
-              </button>
-            </li>
-          {/each}
-          <!-- {:catch error} <p style="color: red">{error.message}</p> -->
-        {/await}
-      </ul>
-    {/if}
+    <ul>
+      {#each features as feature, index}
+        <li class={index == softFocusIndex ? 'softFocus' : ''}>
+          <button
+            on:click={() => {
+              handleClick(feature)
+            }}
+            class="result"
+            tabindex="0"
+          >
+            {feature.properties.label}
+          </button>
+        </li>
+      {/each}
+    </ul>
   </div>
 </div>
 
@@ -165,24 +213,24 @@
       border: none;
       outline: none;
     }
-  }
 
-  .results {
-    max-height: 48vh;
-    padding: 1.5rem;
-    background-color: white;
-    overflow-y: auto;
-    scrollbar-width: thin;
+    & .results {
+      max-height: 48vh;
+      padding: 1.5rem;
+      background-color: white;
+      overflow-y: auto;
+      scrollbar-width: thin;
 
-    & ul {
-      & li {
-        padding: 1rem;
-      }
+      & ul {
+        & li {
+          padding: 1rem;
+        }
 
-      & li.softFocus {
-        border-radius: 0.5rem;
-        outline: 1px solid black;
-        /* background-color: #f6f6f6; */
+        & li.softFocus {
+          border-radius: 0.5rem;
+          outline: 1px solid black;
+          /* background-color: #f6f6f6; */
+        }
       }
     }
   }
