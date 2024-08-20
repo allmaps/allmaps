b5d0a16ac13b6852d9bce6192edf3f3b672ffcb2
Christopher Pietsch <cpietsch@gmail.com>
2024-08-20 13:26:41 +0000

    added dynamic import for SSL

--- apps/viewer/vite.config.ts
+++ apps/viewer/vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 
 import { sveltekit } from '@sveltejs/kit/vite'
-import basicSsl from '@vitejs/plugin-basic-ssl'
-// This is needed to serve the app over https
-// if you are using an IP address instead of localhost to make sure
-// crypto has SubtleCrypto which is needed in the @allmaps/id package
 
 import ports from '../../ports.json'
 
@@ -14,7 +10,10 @@ export default defineConfig({
   },
   plugins: [
     sveltekit(),
+    // This is needed to serve the app over https
+    // if you are using an IP address instead of localhost to make sure
+    // crypto has SubtleCrypto which is needed in the @allmaps/id package
     // @ts-expect-error: Vite types are not up to date
-    process.env.SSL && basicSsl()
+    process.env.SSL && import('@vitejs/plugin-basic-ssl').then((mod) => mod.default())
   ]
 })
