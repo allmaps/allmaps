07f71337fd50fdcf12b06a7dfa82809dfe34afea
Bert Spaan <bert.spaan@gmail.com>
2024-09-05 17:35:06 +0200

    fixed custom transformation bug

--- apps/cli/src/commands/script/geotiff.ts
+++ apps/cli/src/commands/script/geotiff.ts
@@ -91,7 +91,6 @@ export default function generate() {
         imageFilename,
         basename,
         options.outputDir,
-        map,
         gcps,
         polygon,
         transformationType,
