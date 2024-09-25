668ca505e0c07cc95c9a26ee344dcc953b02efab
Bert Spaan <bert.spaan@gmail.com>
2024-09-25 17:08:36 +0200

    static adapter for editor

--- apps/editor/svelte.config.js
+++ apps/editor/svelte.config.js
@@ -1,11 +1,18 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+  preprocess: vitePreprocess({
+    postcss: true
+  }),
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false
+    })
   }
 }
 
