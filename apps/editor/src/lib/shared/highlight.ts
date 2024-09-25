524587db3a77ed5fd399bd0487c92e1310090bd5
Bert Spaan <bert.spaan@gmail.com>
2024-09-25 16:57:34 +0200

    no top level await

--- apps/editor/src/lib/shared/highlight.ts
+++ apps/editor/src/lib/shared/highlight.ts
@@ -1,16 +1,24 @@
-import { createHighlighterCore } from 'shiki/core'
+import { createHighlighterCore, type HighlighterCore } from 'shiki/core'
 import getWasm from 'shiki/wasm'
 import nord from 'shiki/themes/nord.mjs'
 
-const highlighter = await createHighlighterCore({
-  themes: [nord],
-  langs: [import('shiki/langs/json.mjs')],
-  loadWasm: getWasm
-})
+let highlighter: HighlighterCore | undefined
+
+async function initialize() {
+  highlighter = await createHighlighterCore({
+    themes: [nord],
+    langs: [import('shiki/langs/json.mjs')],
+    loadWasm: getWasm
+  })
+}
 
 export function highlight(json: unknown) {
-  return highlighter.codeToHtml(JSON.stringify(json, null, 2), {
-    lang: 'json',
-    theme: 'nord'
-  })
+  if (highlighter) {
+    return highlighter.codeToHtml(JSON.stringify(json, null, 2), {
+      lang: 'json',
+      theme: 'nord'
+    })
+  }
+
+  return ''
 }
