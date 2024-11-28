ae32a3237fd4f4d3a34343c784b327f0d8e7d3a5
Brandon Liu <bdon@bdon.org>
2024-11-28 10:44:25 +0800

    change basemap font to Roboto, remove unused theme

--- packages/basemap/src/colors.ts
+++ packages/basemap/src/colors.ts
@@ -1,4 +1,4 @@
-export const ALLMAPS_THEME_2A = {
+export const ALLMAPS_THEME = {
   background: 'rgba(255, 252, 244, 1)',
   earth: 'rgba(255, 252, 244, 1)',
   park_a: '#e0eec9',
@@ -85,9 +85,9 @@ export const ALLMAPS_THEME_2A = {
   state_label_halo: 'rgba(0,0,0,0)',
   country_label: 'rgba(143, 140, 133, 1)',
 
-  regular: 'Jost Regular',
-  bold: 'Jost Medium',
-  italic: 'Jost Italic',
+  regular: 'Roboto Regular',
+  bold: 'Roboto Medium',
+  italic: 'Roboto Italic',
 
   landcover: {
     grassland: 'rgba(224, 238, 205, 1)',
@@ -97,108 +97,6 @@ export const ALLMAPS_THEME_2A = {
     glacier: 'rgba(255, 249, 235, 1)',
     scrub: 'rgba(238, 238, 193, 1)',
     forest: 'rgba(209, 233, 188, 1)'
-  }
-}
-
-export const ALLMAPS_THEME_5 = {
-  background: 'rgba(255, 252, 247, 1)',
-  earth: 'rgba(255, 252, 247, 1)',
-  park_a: '#bfe4cc',
-  park_b: '#bfe4cc',
-  hospital: 'rgba(255, 236, 247, 1)',
-  industrial: 'rgba(249, 247, 241, 1)',
-  school: 'rgba(255, 243, 237, 1)',
-  wood_a: '#d2ebcd',
-  wood_b: '#d2ebcd',
-  pedestrian: '#e3e0d4',
-  scrub_a: '#d8eedf',
-  scrub_b: '#d8eedf',
-  glacier: '#ffffff',
-  sand: 'rgba(255, 245, 221, 1)',
-  beach: 'rgba(255, 245, 221, 1)',
-  aerodrome: 'rgba(252, 244, 244, 1)',
-  runway: '#e9e9ed',
-  water: 'rgba(211, 244, 248, 1)',
-  pier: '#e0e0e0',
-  zoo: 'rgba(222, 239, 205, 1)',
-  military: 'rgba(249, 246, 243, 1)',
-
-  tunnel_other_casing: '#e0e0e0',
-  tunnel_minor_casing: '#e0e0e0',
-  tunnel_link_casing: '#e0e0e0',
-  tunnel_medium_casing: '#e0e0e0',
-  tunnel_major_casing: '#e0e0e0',
-  tunnel_highway_casing: '#e0e0e0',
-  tunnel_other: '#d5d5d5',
-  tunnel_minor: '#d5d5d5',
-  tunnel_link: '#d5d5d5',
-  tunnel_medium: '#d5d5d5',
-  tunnel_major: '#d5d5d5',
-  tunnel_highway: '#d5d5d5',
-
-  transit_pier: '#e0e0e0',
-  buildings: 'rgba(225, 216, 196, 1)',
-
-  minor_service_casing: 'rgba(255, 252, 247, 1)',
-  minor_casing: 'rgba(255, 252, 247, 1)',
-  link_casing: 'rgba(255, 252, 247, 1)',
-  medium_casing: 'rgba(255, 252, 247, 1)',
-  major_casing_late: 'rgba(255, 252, 247, 1)',
-  highway_casing_late: 'rgba(255, 252, 247, 1)',
-  other: 'rgba(184, 184, 184, 1)',
-  minor_service: 'rgba(184, 184, 184, 1)',
-  minor_a: '#b8b8b8',
-  minor_b: '#b8b8b8',
-  link: 'rgba(184, 184, 184, 1)',
-  medium: 'rgba(184, 184, 184, 1)',
-  major_casing_early: 'rgba(255, 252, 247, 1)',
-  major: 'rgba(150, 153, 195, 1)',
-  highway_casing_early: 'rgba(255, 252, 247, 1)',
-  highway: 'rgba(150, 153, 195, 1)',
-
-  railway: '#a7b1b3',
-  boundaries: '#adadad',
-  waterway_label: '#ffffff',
-
-  bridges_other_casing: 'rgba(255, 252, 247, 1)',
-  bridges_minor_casing: 'rgba(255, 252, 247, 1)',
-  bridges_link_casing: 'rgba(255, 252, 247, 1)',
-  bridges_medium_casing: 'rgba(255, 252, 247, 1)',
-  bridges_major_casing: 'rgba(255, 252, 247, 1)',
-  bridges_highway_casing: 'rgba(255, 252, 247, 1)',
-  bridges_other: 'rgba(184, 184, 184, 1)',
-  bridges_minor: 'rgba(184, 184, 184, 1)',
-  bridges_link: 'rgba(184, 184, 184, 1)',
-  bridges_medium: 'rgba(184, 184, 184, 1)',
-  bridges_major: 'rgba(184, 184, 184, 1)',
-  bridges_highway: 'rgba(150, 153, 195, 1)',
-
-  roads_label_minor: 'rgba(138, 137, 134, 1)',
-  roads_label_minor_halo: 'rgba(255, 252, 247, 1)',
-  roads_label_major: 'rgba(138, 137, 134, 1)',
-  roads_label_major_halo: 'rgba(255, 252, 247, 1)',
-  ocean_label: '#ffffff',
-  peak_label: '#7e9aa0',
-  subplace_label: '#8f8f8f',
-  subplace_label_halo: '#FFFCF7',
-  city_label: '#5c5c5c',
-  city_label_halo: '#e0e0e0',
-  state_label: 'rgba(162, 161, 159, 1)',
-  state_label_halo: 'rgba(255, 252, 247, 1)',
-  country_label: 'rgba(132, 132, 132, 1)',
-
-  regular: 'Jost Regular',
-  bold: 'Jost Medium',
-  italic: 'Jost Italic',
-
-  landcover: {
-    grassland: 'rgba(210, 239, 207, 1)',
-    barren: 'rgba(255, 243, 215, 1)',
-    urban_area: 'rgba(230, 230, 230, 1)',
-    farmland: 'rgba(216, 239, 210, 1)',
-    glacier: 'rgba(255, 255, 255, 1)',
-    scrub: 'rgba(234, 239, 210, 1)',
-    forest: 'rgba(196, 231, 210, 1)'
   }
 }
 
--- packages/basemap/src/index.ts
+++ packages/basemap/src/index.ts
@@ -2,7 +2,7 @@ import { layersWithCustomTheme } from 'protomaps-themes-base'
 import mlcontour from 'maplibre-contour'
 import { Map } from 'maplibre-gl'
 import { StyleSpecification } from '@maplibre/maplibre-gl-style-spec'
-import { ALLMAPS_THEME_2A, TERRAIN_THEME } from './colors'
+import { ALLMAPS_THEME, TERRAIN_THEME } from './colors'
 
 export function basemapStyle(
   lang: string,
@@ -26,7 +26,7 @@ export function basemapStyle(
         attribution: '© <a href="https://openstreetmap.org">OpenStreetMap</a>'
       }
     },
-    layers: layersWithCustomTheme('protomaps', ALLMAPS_THEME_2A, lang)
+    layers: layersWithCustomTheme('protomaps', ALLMAPS_THEME, lang)
   }
 }
 
