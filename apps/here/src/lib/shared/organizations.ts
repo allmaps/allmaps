8c44b810d00b08cba177186fa2777a6f8a9067a3
Bert Spaan <bert.spaan@gmail.com>
2025-06-18 14:06:43 +0200

    added more organization labels

--- apps/here/src/lib/shared/organizations.ts
+++ apps/here/src/lib/shared/organizations.ts
@@ -1,6 +1,12 @@
 const organisationsByDomain: Record<string, string> = {
   'iiif.nypl.org': 'the New York Public Library',
-  'www.davidrumsey.com': 'the David Rumsey Map Collection'
+  'www.davidrumsey.com': 'the David Rumsey Map Collection',
+  'iiif.digitalcommonwealth.org': 'Digital Commonwealth',
+  'collections.library.yale.edu': 'Yale University Library',
+  'service.archief.nl': 'the National Archives of the Netherlands',
+  'cdm21033.contentdm.oclc.org': 'Vrije Universiteit Amsterdam',
+  'iiif.universiteitleiden.nl': 'Leiden University Libraries',
+  'images.uba.uva.nl': 'UvA Bijzondere Collecties / Allard Pierson Museum'
 }
 
 export function organizationNameFromImageServiceDomain(domain: string) {
