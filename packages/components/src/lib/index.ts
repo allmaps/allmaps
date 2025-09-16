c90917e6db331b826ccfc4da84a932317e2e90a6
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2025-09-16 16:47:24 +0200

    Remove map and options components and their dependencies

--- packages/components/src/lib/index.ts
+++ packages/components/src/lib/index.ts
@@ -1,5 +1,7 @@
 import Geocoder from '$lib/components/Geocoder.svelte'
-import ProjectionPicker from '$lib/components/options/ProjectionPicker.svelte'
+import ProjectionPicker from '$lib/components/ProjectionPicker.svelte'
+import TransformationTypePicker from '$lib/components/TransformationTypePicker.svelte'
+import DistortionMeasurePicker from '$lib/components/DistortionMeasurePicker.svelte'
 
 import { GeocodeEarthGeocoderProvider } from '$lib/shared/geocoder/providers/geocode-earth.js'
 import { WorldHistoricalGazetteerGeocoderProvider } from '$lib/shared/geocoder/providers/world-historical-gazetteer.js'
@@ -8,7 +10,12 @@ import { getUrlState, setUrlState } from '$lib/state/url.svelte.js'
 
 import type { GeocoderGeoJsonFeature } from '$lib/shared/geocoder/types.js'
 
-export { Geocoder, ProjectionPicker }
+export {
+  Geocoder,
+  ProjectionPicker,
+  TransformationTypePicker,
+  DistortionMeasurePicker
+}
 export {
   GeocodeEarthGeocoderProvider,
   WorldHistoricalGazetteerGeocoderProvider
--- packages/components/src/routes/+page.svelte
+++ packages/components/src/routes/+page.svelte
@@ -1,16 +1,10 @@
 <script lang="ts">
-  import { parseAnnotation } from '@allmaps/annotation'
-
-  import ProjectionPicker from '$lib/components/options/ProjectionPicker.svelte'
+  import ProjectionPicker from '$lib/components/ProjectionPicker.svelte'
   import projectionsData from '$lib/shared/projections/projections.json' with { type: 'json' }
   import {
     createSearchProjectionsWithFuse,
     createSuggestProjectionsWithFlatbush
   } from '$lib/shared/projections/projections.js'
-  import WarpedMapLayerMap from '$lib/components/maps/WarpedMapLayerMap.svelte'
-
-  import type { GeoreferencedMap } from '@allmaps/annotation'
-  import Viewer from '$lib/components/Viewer.svelte'
 
   const projections = projectionsData.map((projectionData) => {
     return {
@@ -26,20 +20,6 @@
   const searchProjectionsWithFuse = createSearchProjectionsWithFuse(projections)
   const suggestProjectionsWithFlatbush =
     createSuggestProjectionsWithFlatbush(projections)
-
-  // Diemer meer
-  const annotationUrl =
-    'https://annotations.allmaps.org/manifests/a0d6d3379cfd9f0a'
-
-  let annotations = $state<unknown[]>([])
-  let georeferencedMaps = $state<GeoreferencedMap[]>([])
-
-  fetch(annotationUrl)
-    .then((response) => response.json())
-    .then((response) => {
-      annotations = [response]
-      georeferencedMaps = parseAnnotation(response)
-    })
 </script>
 
 <main class="container mx-auto p-4 space-y-8">
@@ -54,18 +34,5 @@
       geoBbox={[4.01001, 50.762522, 4.523621, 51.024121]}
       suggestProjections={suggestProjectionsWithFlatbush}
     ></ProjectionPicker>
-  </section>
-  <section>
-    <h1 class="text-xl font-bold mb-4">Map</h1>
-    <div class="size-100">
-      <WarpedMapLayerMap {georeferencedMaps} />
-    </div>
-  </section>
-
-  <section>
-    <h1 class="text-xl font-bold mb-4">Viewer</h1>
-    <div class="size-200">
-      <Viewer {annotations} />
-    </div>
   </section>
 </main>
