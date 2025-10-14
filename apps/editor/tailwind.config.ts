f9a63a3f1ebca41dd23376c3b90ee3a4a4fee957
Bert Spaan <bert.spaan@gmail.com>
2025-10-14 12:07:25 +0200

    cleaned up conf files

--- apps/editor/tailwind.config.ts
+++ apps/editor/tailwind.config.ts
@@ -1,5 +1,4 @@
 import { theme } from '@allmaps/tailwind'
-import typography from '@tailwindcss/typography'
 
 import type { Config } from 'tailwindcss'
 
@@ -9,5 +8,5 @@ export default {
     './node_modules/@allmaps/components/dist/components/**/*.{html,js,svelte,ts}'
   ],
   theme,
-  plugins: [typography]
+  plugins: []
 } satisfies Config
