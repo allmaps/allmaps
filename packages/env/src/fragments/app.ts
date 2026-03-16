efc2ded8e7ec6d2c20163b94140c8a5abb485666
Bert Spaan <bert.spaan@gmail.com>
2026-03-16 16:35:24 +0100

    made app-specific env vars optional

--- packages/env/src/fragments/app.ts
+++ packages/env/src/fragments/app.ts
@@ -5,10 +5,10 @@ import { envBoolean } from '../shared.js'
 
 export const appPublicEnvSchema = urlsEnvSchema.merge(
   z.object({
-    PUBLIC_STATS_WEBSITE_ID: z.string(),
-    PUBLIC_GEOCODE_EARTH_KEY: z.string(),
-    PUBLIC_BANNER_ENABLED: envBoolean,
-    PUBLIC_BANNER_TEXT: z.string()
+    PUBLIC_STATS_WEBSITE_ID: z.string().optional(),
+    PUBLIC_GEOCODE_EARTH_KEY: z.string().optional(),
+    PUBLIC_BANNER_ENABLED: envBoolean.optional(),
+    PUBLIC_BANNER_TEXT: z.string().optional()
     // VITE_BANNER_ENABLED
     // VITE_BANNER_TEXT
   })
