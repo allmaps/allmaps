42f8dd55d16174a11d3cfbf6aa6d0e7ea26cd9d8
Bert Spaan <bert.spaan@gmail.com>
2026-06-02 16:16:34 +0200

    lowercase keyboard shortcuts in modal

--- apps/viewer/src/lib/components/modals/Keyboard.svelte
+++ apps/viewer/src/lib/components/modals/Keyboard.svelte
@@ -45,11 +45,11 @@
       </tr>
       <tr>
         <td class="p-1">Switch to map view</td>
-        <td class="p-1 whitespace-nowrap"><Kbd>M</Kbd></td>
+        <td class="p-1 whitespace-nowrap"><Kbd>m</Kbd></td>
       </tr>
       <tr>
         <td class="p-1">Switch to image view</td>
-        <td class="p-1 whitespace-nowrap"><Kbd>I</Kbd></td>
+        <td class="p-1 whitespace-nowrap"><Kbd>i</Kbd></td>
       </tr>
 
       <tr class="border-b-2 border-pink font-bold">
@@ -61,7 +61,7 @@
       </tr>
       <tr>
         <td class="p-1">Temporarily remove background color</td>
-        <td class="p-1 whitespace-nowrap"><Kbd>B</Kbd></td>
+        <td class="p-1 whitespace-nowrap"><Kbd>b</Kbd></td>
       </tr>
       <tr>
         <td class="p-1">Select map</td>
