61bce9764cfe27be5e1ac969aade7c7dbc42d138
Bert Spaan <bert.spaan@gmail.com>
2026-06-16 12:23:30 +0200

    increase the fetch limit for apps/console

--- apps/console/src/routes/users/+page.server.ts
+++ apps/console/src/routes/users/+page.server.ts
@@ -1,10 +1,11 @@
 import { restFetch } from '$lib/server/rest.js'
+import { CONSOLE_LIST_LIMIT } from '$lib/limits.js'
 
 import type { ConsoleUser } from './users.remote.js'
 import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async () => {
   return {
-    users: await restFetch<ConsoleUser[]>('/users?limit=10000')
+    users: await restFetch<ConsoleUser[]>(`/users?limit=${CONSOLE_LIST_LIMIT}`)
   }
 }
