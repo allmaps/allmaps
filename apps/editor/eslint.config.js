fda75c033c2ae3d70692d74e3744b5d674d8dac8
Bert Spaan <bert.spaan@gmail.com>
2026-07-18 19:02:00 +0200

    chore: migrate Svelte workspaces to ESLint 10

--- apps/editor/eslint.config.js
+++ apps/editor/eslint.config.js
@@ -1,28 +1,22 @@
 import prettier from 'eslint-config-prettier'
-import { fileURLToPath } from 'node:url'
-import { includeIgnoreFile } from '@eslint/compat'
+import path from 'node:path'
 import js from '@eslint/js'
 import svelte from 'eslint-plugin-svelte'
-import { defineConfig } from 'eslint/config'
+import { defineConfig, includeIgnoreFile } from 'eslint/config'
 import globals from 'globals'
 import ts from 'typescript-eslint'
-import svelteConfig from './svelte.config.js'
 
-const gitignorePath = fileURLToPath(
-  new URL('./../../.gitignore', import.meta.url)
-)
+const gitignorePath = path.resolve(import.meta.dirname, '../../.gitignore')
 
 export default defineConfig(
   includeIgnoreFile(gitignorePath),
   js.configs.recommended,
-  ...ts.configs.recommended,
-  ...svelte.configs.recommended,
+  ts.configs.recommended,
+  svelte.configs.recommended,
   prettier,
-  ...svelte.configs.prettier,
+  svelte.configs.prettier,
   {
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node }
-    },
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
     rules: {
       // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
       // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
@@ -35,10 +29,11 @@ export default defineConfig(
       parserOptions: {
         projectService: true,
         extraFileExtensions: ['.svelte'],
-        parser: ts.parser,
-        svelteConfig
+        parser: ts.parser
       }
-    },
+    }
+  },
+  {
     rules: {
       'svelte/prefer-svelte-reactivity': 'warn',
       'svelte/no-navigation-without-resolve': 'warn'
